89fb1dcd523507b3c0b7b47d91c0bd41df3fbf6b
jenmar02 <156864399+jenmar02@users.noreply.github.com>
2024-09-08 22:07:37 -0500

    added the api intergration ai

--- src/script.js
+++ src/script.js
@@ -1,40 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-  alert("Generating poem");
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter(".poem-container", {
-    strings: `No te amo como si fueras rosa de sal, topacio
-          <br />
-          o flecha de claveles que propagan el fuego:
-          <br />
-          te amo como se aman ciertas cosas oscuras,
-          <br />
-          secretamente, entre la sombra y el alma.
-          <br /><br />
-          Te amo como la planta que no florece y lleva
-          <br />
-          dentro de sí, escondida, la luz de aquellas flores,
-          <br />
-          y gracias a tu amor vive oscuro en mi cuerpo
-          <br />
-          el apretado aroma que ascendió de la tierra.
-          <br /><br />
-          Te amo sin saber cómo, ni cuándo, ni de dónde
-          <br />
-          te amo directamente sin problemas ni orgullo:
-          <br />
-          así te amo porque no sé amar de otra manera,
-          <br /><br />
-          sino así de este modo en que no soy ni eres,
-          <br />
-          tan cerca que tu mano sobre mi pecho es mía,
-          <br />
-          tan cerca que se cierran tus ojos con mi sueño.
-          <br />`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionInput = doocument.querySelector("#user-instructions");
+  let apiKey = "e49t4e467d35faocb8ec3a1644a604fa";
+  let prompt = `User instructions: Generate a French poem about ${instructionInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poem. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions below. Do not include a title of the poem. Sign the poem with 'SheCodes AI' inside a <strong> element and font-style italic. Make sure to sign at the end of the poem and NOT at the beginning. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generated poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Contex: ${context}`);
+  axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
