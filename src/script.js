d8677771499dae3fcd24f5ed08f6cccc0f54f98e
jenmar02 <156864399+jenmar02@users.noreply.github.com>
2024-09-08 22:38:50 -0500

    attempted to add an english translation to the spanish poem

--- src/script.js
+++ src/script.js
@@ -6,6 +6,22 @@ function displayPoem(response) {
     delay: 1,
     cursor: "",
   });
+  new Typewriter("#eng-poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+function updateEnglishPoem(event) {
+  event.preventDefault();
+  let instructions = document.querySelector("#instructions");
+  let apiKey = "e49t4e467d35faocb8ec3a1644a604fa";
+  let context =
+    "Your mission is to translate the 4 line poem generated above in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions from before. Do not include a title of the poem. Sign the poem with 'SheCodes AI' inside a <strong> element and font-style italic. Make sure to sign at the end of the poem and NOT at the beginning. ";
+  let prompt = `User instructions: Translate the poem the Spanish poem about ${instructions.value} to the english language`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
 function generatePoem(event) {
   event.preventDefault();
@@ -16,10 +32,9 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a Spanish poem about ${instructions.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating poem");
-  console.log(context);
-  console.log(prompt);
+  updateEnglishPoem();
   axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
+poemFormElement.addEventListener("submit", updateEnglishPoem);
